99ed1f057f6e927f7a6b2f488692ad66993da17e
Wesley Henrique Francisco <wesley.hf93@hotmail.com>
2016-10-03 01:07:04 -0300

    login page added [wip]

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,28 @@
 import { Component } from '@angular/core';
-
 import { NavController } from 'ionic-angular';
 
+import { LoginPage } from '../login/login';
+
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
+    user: any = {};
 
-  constructor(public navCtrl: NavController) {
-    
-  }
+    constructor(public navCtrl: NavController) {
+        // this.pushPage = LoginPage;
+        // this.params = {};
+    }
 
+    ngOnInit() {
+        console.log('page home init');
+
+        // check logged uer
+        // if (this.user.name) {
+            
+        // } else {
+        //     this.navCtrl.push(LoginPage);
+        // }
+    }
 }
---
+++ src/pages/login/login.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+    selector: 'page-login',
+    templateUrl: 'login.html'
+})
+export class LoginPage {
+
+    constructor(public navCtrl: NavController) {}
+
+    ionViewDidLoad() {
+        console.log('Hello Login Page');
+    }
+
+}
